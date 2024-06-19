527bb5561687c601221bdf8442ac59ad1e1b8c43
asheessharma <ashees.sharma_cs20@gla.ac.in>
2024-06-19 08:57:37 +0530

    Initial commit

---
+++ src/TreadsJava.java
@@ -0,0 +1,28 @@
+class MyThread extends Thread{
+    public void run(){
+        for(int i=-10;i<200;i++){
+            System.out.println("created extending Thread class");
+        }
+    }
+}
+class AnotherThread implements Runnable{
+    public void run(){
+        for(int i=-10;i<200;i++){
+            System.out.println("I was created using Runnable interface");
+        }
+
+    }
+
+}
+
+public class TreadsJava {
+    public static void main(String[] args) {
+        MyThread obj = new MyThread();
+        AnotherThread t2 = new AnotherThread();
+        Thread obj2 = new Thread(t2);
+        obj.start();
+        obj2.start();
+
+    }
+
+}
