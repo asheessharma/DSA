d5cf4df966e4217b093b9256558ce7a8dbdffdc1
asheessharma <ashees.sharma_cs20@gla.ac.in>
2024-08-09 07:16:44 +0530

    Initial commit

--- src/Stack_Queue/InfixToPostfix.java
+++ src/Stack_Queue/InfixToPostfix.java
@@ -1,4 +1,59 @@
 package Stack_Queue;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class InfixToPostfix {
+    static int Prec(char ch){
+        return switch (ch) {
+            case '+', '-' -> 1;
+            case '*', '/' -> 2;
+            case '^' -> 3;
+            default -> -1;
+        };
+    }
+
+    private static void infix2postfix(String str){
+        int n = str.length();
+        char [] arr = str.toCharArray();
+        Stack<Character> s = new Stack<>();
+        int i = 0;
+        String ans = "";
+        while(i < n){
+            if((arr[i] >= 'a' && arr[i]<='z')||(arr[i] >= 'A' && arr[i]<='Z') ||(arr[i] >= '1' && arr[i]<='9')){
+                ans+=arr[i];
+            }
+            else if(arr[i]=='('){
+                s.push(arr[i]);
+            }
+            else if(arr[i] == ')'){
+                while(!s.empty() && s.peek()!='('){
+                    ans += s.pop();
+                }
+                s.pop();
+            }
+            else{
+                while(!s.empty() && Prec(arr[i])<= Prec(s.peek())){
+                        ans += s.pop();
+                }
+                s.push(arr[i]);
+            }
+            i++;
+        }
+        while(!s.empty()){
+            if(s.peek()=='('){
+                System.out.println("Invalid Expression");
+                return;
+            }
+            ans+=s.pop();
+        }
+        System.out.println("Resulting Postfix expression: "+ ans);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        infix2postfix(str);
+
+    }
 }
