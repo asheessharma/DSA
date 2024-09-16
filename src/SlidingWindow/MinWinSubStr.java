b1ec9e410818af8dbb9da7f77b584d280ffda898
asheessharma <ashees.sharma_cs20@gla.ac.in>
2024-09-16 15:30:29 +0530

    Initial commit

--- src/SlidingWindow/MinWinSubStr.java
+++ src/SlidingWindow/MinWinSubStr.java
@@ -8,35 +8,44 @@ public class MinWinSubStr {
         int i = 0;
         int j = 0;
         int n = str.length();
+        int startIndex = 0;
         HashMap<Character, Integer> map = new HashMap<>();
         for(int k = 0; k < t.length(); k++){
             char temp = t.charAt(k);
-            if(map.containsKey(temp)){
-                map.put(temp,map.get(temp)+1);
-            }
-            else{
-                map.put(temp,1);
-            }
+            map.put(temp,map.getOrDefault(temp,0)+1);
         }
         int count = map.size();
-        int mini = 0;
+        int mini = Integer.MAX_VALUE;
         while(j < n){
             char ch = str.charAt(j);
             if(map.containsKey(ch)){
                 map.put(ch, map.get(ch)-1);
+                if(map.get(ch) == 0){
+                    count--;
+                }
             }
-            if(map.get(ch) == 0){
-                count--;
-            }
-        }
-        if(count == 0){
-            mini = Math.min(mini, (j-i+1));
-        }
+            while(count == 0){
+                    if((j-i+1)< mini){
+                        mini = j-i+1;
+                        startIndex = i;
+                    }
+                    char c = str.charAt(i);
+                    if(map.containsKey(c)){
+                        map.put(c,map.get(c)+1);
+                        if(map.get(c)>0){
+                            count++;
+                        }
+                    }
+                    i++;
+                }
 
-        j++;
+            j++;
+        }
+        System.out.println("Minimum Window Size: " + str.substring(startIndex,startIndex+mini));
+        System.out.println("Size : " + mini);
     }
 
     public static void main(String[] args) {
-
+        MinWindow("abaaxcxcabaxxbc","abc");
     }
 }
