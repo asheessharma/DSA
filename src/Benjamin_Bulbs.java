39dfaedeb9a04ff111846073a22f1d0b84af65e3
asheessharma <ashees.sharma_cs20@gla.ac.in>
2024-10-19 15:41:07 +0530

    Initial commit

--- src/Benjamin_Bulbs.java
+++ src/Benjamin_Bulbs.java
@@ -1,16 +1,33 @@
-
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Benjamin_Bulbs {
-    private static void solution_benjamin( int n){
-
-        for(int i=1 ;i*i <= n ; i++){
-            System.out.println( i*i );
+    private static int[] solution_benjamin(int [] arr1, int n){
+        for(int i=2; i <= n ; i++){
+            for(int j = i; j <= n ; j++){
+                if(j % i == 0){
+                    if(arr1[j-1]==0){
+                        arr1[j-1]=1;
+                    }else{
+                        arr1[j-1] = 0;
+                    }
+                }
+            }//Running this would tell that only perfect square till that number be on reset will be off.
+        }
+        return arr1;
+    }
+    private static void optimised(int n){
+        for(int i = 1 ; i*i <= n ;i++){
+            System.out.println(i*i);
         }
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        solution_benjamin(n);
+//        int [] bulbs = new int[n];
+//        Arrays.fill(bulbs,1);
+//        System.out.println(Arrays.toString(solution_benjamin(bulbs,n)) );
+        optimised(n);
     }
 }
